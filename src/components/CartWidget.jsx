555a893f8b9dffa7cd8a9d0472e5ea348dafc486
Matias Valdez <matiasvaldez33@gmail.com>
2023-07-17 23:02:18 -0300

    Styling

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FaCartShopping } from "react-icons/fa6";
+import './cartWidget.css'
 
 const CartWidget = () => {
   return (
-    <div>
-        <FaCartShopping/>
-        <p>3</p>
+    <div class="cart">
+        <button class="carritoLogo"><FaCartShopping size={25}/></button>
+        <span class="contadorCarrito">3</span>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
+import './navBar.css'
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Entre Hilos</Navbar.Brand>
+          <Navbar.Brand href="#home"><img class="imgLogo" src="src/assets/images/logo/logo.jpg"
+          alt="Entre Hilos Logo"/></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Inicio</Nav.Link>
             <Nav.Link href="#features">Nosotros</Nav.Link>
