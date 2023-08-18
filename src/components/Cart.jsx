6df8d95cb78a17ecf3eb120fddbf4e898451b796
Matias Valdez <matiasvaldez33@gmail.com>
2023-08-17 21:15:40 -0300

    Carrito de compras

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,8 +14,35 @@ const Cart = () => {
 
   return (
     <div>
-      {cantidadTotal < 1 ? <p>no hay un pingo</p> : <p>Hay elementos</p>}
+      {cantidadTotal < 1 ? (
+        <p>No hay productos en el carrito</p>
+      ) : (
+        <div>
+          <h2>Carrito</h2>
+          <p className="carrito__vacio">El carrito está vacío</p>
+
+          <div className="articulos__carrito">
+            <div className="articulo__carrito"></div>
+          </div>
+
+          <div className="carrito__acciones">
+            <button className="carrito__acciones__vaciar">
+              Vaciar Carrito
+            </button>
+            <div className="carrito__total">
+              <p className="precio__total">Total:</p>
+              <p className="precio__total" id="total">
+                {/* Agrega aquí la lógica para mostrar el total */}
+              </p>
+            </div>
+            <button className="carrito__acciones__comprar">
+              Finalizar compra
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Cart;
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../context/ShoppingCartContext";
 import { FaCartShopping } from "react-icons/fa6";
-import '../index.css'
+import "../index.css";
 
 const CartWidget = () => {
+  const { cart } = useContext(CartContext);
   return (
     <div className="cart">
-        <button className="carritoLogo"><FaCartShopping size={28}/></button>
-        <span className="contadorCarrito">3</span>
+      <button className="carritoLogo">
+        <FaCartShopping size={28} />
+      </button>
+      <span className="contadorCarrito">{cart.length}</span>
     </div>
-  )
-}
+  );
+};
 
-export default CartWidget
+export default CartWidget;
